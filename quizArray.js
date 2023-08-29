2e55419344f5709b5f275df5289780284eacb448
Andres Marquez <andresmr955@gmail.com>
2023-08-29 04:06:19 -0400

    Quiz array

---
+++ quizArray.js
@@ -0,0 +1,12 @@
+var arraySecreto = ["Huevo", "Gallina", "Vaca"];
+function solution(arraySecreto) {
+    
+return typeof arraySecreto[0] === "string";}
+solution(arraySecreto);
+
+
+var arraySecreto = ["Huevo", "Gallina", "Vaca"];
+function solution(arraySecreto) {
+    
+return typeof arraySecreto[0] === "string"? true : false;}
+solution(arraySecreto);
